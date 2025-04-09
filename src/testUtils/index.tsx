dcd74e3cb674499e3289dc6f723c3005bc0d1ee9
Nat <natalia.km47@gmail.com>
2025-04-09 23:30:29 +0100

    feat: fix tests v2

--- src/testUtils/index.tsx
+++ src/testUtils/index.tsx
@@ -1,20 +1,20 @@
 import { PropsWithChildren } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient =  new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: false,
-            refetchOnMount: false,
-            refetchOnReconnect: false,
-            refetchOnWindowFocus: false,
-            enabled: true
+const getQueryClient = () => {
+    return new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false,
+                refetchOnMount: false,
+                refetchOnReconnect: false,
+                refetchOnWindowFocus: false
+            }
         }
-    }
-})
-
+    })
+}
 export const userContextAndQueryWrapper = ({ children }: PropsWithChildren) => (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={getQueryClient()}>
         {children}
     </QueryClientProvider>
 )
